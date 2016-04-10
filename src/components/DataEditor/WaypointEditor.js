1303d82cb97c93a6d0be87c439449914ba6128c0
Maxime Warnier <marmax@gmail.com>
2016-04-10 09:57:29 +0200

    complete waypoint view

--- src/components/DataEditor/WaypointEditor.js
+++ src/components/DataEditor/WaypointEditor.js
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import {OBJECTS,WP_DATATYPES} from "../../datatypes.constants.js"
 
       // {data.map((waypoint) => <li key={waypoint.name}>{waypointGroup.get("name")}</li>)}
+      //
 
 const WaypointEditor = ({data}) => {
-  console.log("--->",data.toJS())
+
   return <div>
-    <ul>...
+    <div>Group Name : {data.get("groupName")}</div>
+
+    <ul>
+      {data.get("data").map((waypoint,i) => <li key={i}>
+        {waypoint.get(WP_DATATYPES.TYPE_NAME)} - {waypoint.get(WP_DATATYPES.TYPE_LAT)} / {waypoint.get(WP_DATATYPES.TYPE_LNG)}
+        </li>)}
     </ul>
 
   </div>
